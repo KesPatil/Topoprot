7de135406fcbd7a31cebd0f9a30aa83c3d39e9e2
Keshav_Patil <patilk@seas.upenn.edu>
2023-01-03 13:17:55 -0500

    initial commit

---
+++ topoprot/add.py
@@ -0,0 +1,11 @@
+
+
+
+def betti_num():
+    """
+       :param float num1: I take num1
+       :param float num2: I take num1
+       :returns: betti numbers
+       :rtype: float 
+    """
+    print("I calculate and print betti numbers")
---
+++ topoprot/homology.py
@@ -0,0 +1,8 @@
+def some_algebra(num1, num2):
+    """
+       :param float num1: I take num1
+       :param float num2: I take num1
+       :returns: some algebra
+       :rtype: float 
+    """
+    print("I do some algebraic topology")
